9b5f3034f8af04144ca4bf27a3067ffb6a874e01
qiliu <liu.qi.alex@gmail.com>
2016-05-18 22:08:57 -0400

    add read

--- arm.py
+++ arm.py
@@ -4,21 +4,36 @@ import warnings
 import numpy as np
 
 NUM_SERVO = 7; #should not be changed
+class Flag:
+    begin = 0xFE
+    status = 0xF5
+    speed = 0xF7
+    angle = 0xF9
+    IK6 = 0xFA
+    IK5 = 0xFB
+    IK3 = 0xFC
+    read = 0xF9
+    mask = 0x7F
+
 class Arm:
     def __init__(self):
         try:
-            self.port = serial.Serial(config.PORT_NAME, config.BAUD_RATE);
+            self.port = serial.Serial(config.PORT_NAME, config.BAUD_RATE, timeout=1);
         except x:
             warnings.warn("can't open the serial port to control the robot arm");
             raise x;
 
+        #to be set to control the arm
         self.fluent = np.array([True]*NUM_SERVO, dtype=np.bool);
         self.speed = np.array([50]*NUM_SERVO, dtype=np.int);
-        self._doFluentSpeed();
         self.angle = np.array([90,145,70,90,90,90,45],dtype=np.float32);
         assert(len(self.angle) == NUM_SERVO);
-        self.isAllConverged = True;
+        self._doFluentSpeed();
         self._doAngle();
+        self.isAllConverged = True;
+        #read from the arm
+        self._force = np.array([0]*NUM_SERVO, dtype=np.int);
+        self._pos = np.array([0]*NUM_SERVO, dtype=np.float32);
 
     def __del__(self):
         self.port.close()
@@ -26,23 +41,23 @@ class Arm:
     def _do(self, command):
         self.port.write(command)
 
-    def _doFluentSpeed(self):#can we do one at a time?? 
+    def _doFluentSpeed(self):
         s = np.clip(self.speed,0,250)//10;
         s[self.fluent] += 64;
-        command = bytes([0xFE,0xF7] + [0x7F & x for x in s]);
+        command = bytes([Flag.begin,Flag.speed] + [Flag.mask & x for x in s]);
         self._do(command)
 
-    def _doAngle(self): #can we do one at a time?
+    def _doAngle(self): 
         self.isAllConverged = False
         c = np.array(self.angle * 50/9, dtype = np.int16)
-        command = [0xFE,0xF9]
+        command = [Flag.begin,Flag.angle]
         for x in c:
-            command += [(x//128) & 0x7F, x & 0x7F]
+            command += [(x//128) & Flag.mask, x & Flag.mask]
         command = bytes(command)
         self._do(command)
 
     def setForceStatus(self, status):#0-forceless, 1-normal, 2-protected
-        c = bytes([0xFE, 0xF5, 0x7F & status]);
+        c = bytes([Flag.begin, Flag.status, Flag.mask & status]);
         self._do(c)
 
     def setSpeed(self,servo_speed):  #set servo speed , 0-250
@@ -61,4 +76,25 @@ class Arm:
         ##shoudn't we limit the range of the angels here!!
         self._doAngle();
 
-    ## need to add serialEvent to read from the servos to check if the move is done or not
+    def read(self):#get force and position, needs to keep updating or else it won't return the laste force and position!!
+        data = bytes(2*NUM_SERVO+1);
+        #read bytes
+        while True:
+            c = self.port.read()[0]
+            if c == Flag.begin:
+                c = self.port.read()[0]
+                if c == Flag.read:
+                    break
+
+        data = self.port.read(len(data))
+
+        if data[-1] == 1:
+            self.isAllConverged = True
+        else:
+            self.isAllConverged = False
+        for i in range(NUM_SERVO):
+            f = data[i*2]>>3;
+            # -7 to 7
+            self._force[i] = (f & 0x07) * (1 if (f>>3)==0 else 0);
+            # 0 to 180 degree
+            self._pos[i] = (9.0/50)*((data[i*2] & 0x07) * 128 + data[i*2+1]);
